e12659109096d3286d411ddbfe438cd7f84df4bf
Orane Orveillon <orane.orveillon@outlook.fr>
2026-02-02 21:45:23 +0100

    Get images from wikipedia local json file

--- server/services/wikipedia.ts
+++ server/services/wikipedia.ts
@@ -1,37 +1,23 @@
-import type { BodyType } from './../types/solar-system'
 import type { WikipediaPageSummaryApi } from '../types/wikipedia-page-summary'
 
 const BASE_URL = 'https://en.wikipedia.org/api/rest_v1/page/summary'
 
 export async function fetchWikipediaImageUrl(
-  bodyName: string,
-  bodyType: BodyType
+  url: string
 ): Promise<string | null> {
-  const searchVariants = [
-    `${bodyName} (${bodyType.toLowerCase()})`,
-    `${bodyType} ${bodyName}`,
-    bodyName
-  ]
+  const pattern = '/wiki/'
+  const query: string = url.substring(url.indexOf(pattern) + pattern.length, url.length)
 
-  for (const variant of searchVariants) {
-    if (!bodyName.startsWith('S/')) {
-      try {
-        const response = await fetch(`${BASE_URL}/${encodeURIComponent(variant)}`, {
-          headers: {
-            'User-Agent': 'AstroWiki/1.0'
-          }
-        })
-
-        const json = await response.json() as WikipediaPageSummaryApi
-
-        if (json.originalimage?.source) {
-          return json.originalimage.source
-        }
-      } catch {
-        continue
-      }
+  const response = await fetch(`${BASE_URL}/${query}`, {
+    headers: {
+      'User-Agent': 'AstroWiki/1.0'
     }
-  }
+  })
 
+  const json = await response.json() as WikipediaPageSummaryApi
+
+  if (json.originalimage?.source) {
+    return json.originalimage.source
+  }
   return null
 }
